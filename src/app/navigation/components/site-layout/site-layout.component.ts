2a2a4838fa49993c8b2cf56840dd09831979a16d
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-04-04 20:16:20 +0300

    added change detection startegy rule, added variables scss, cleaned site-layout component from changed detectionref

--- src/app/navigation/components/site-layout/site-layout.component.ts
+++ src/app/navigation/components/site-layout/site-layout.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { UserRoles } from 'src/app/shared/constants/constants';
@@ -8,23 +8,22 @@ import { sideNavigationLinksData } from '../../constants/constants';
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
   styleUrls: ['./site-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SiteLayoutComponent implements AfterViewInit {
+export class SiteLayoutComponent implements OnInit {
   sideNavigationData = sideNavigationLinksData;
   title = '';
 
   // the role will be selected from the store user data
   role = UserRoles.Admin;
 
-  constructor(
-    private route: ActivatedRoute,
-    private cdref: ChangeDetectorRef,
-  ) {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.setHeader();
+  }
 
   setHeader(): void {
     this.title = this.route.snapshot.firstChild?.data['header'];
-  }
-  ngAfterViewInit(): void {
-    this.cdref.detectChanges();
   }
 }
